8b3e5917847c815045a5d69fbb6a16092a334ebf
cleger97 <chris.legerjr@gmail.com>
2018-04-13 14:37:51 -0400

    Generate Level

--- Assets/Scripts/Level Manager/Elevator Level/GenerateLevel.cs
+++ Assets/Scripts/Level Manager/Elevator Level/GenerateLevel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 
-public class GenerateLevel : MonoBehaviour {
+public class GenerateLevel : ScriptableObject {
 
 	[MenuItem("AdditionalTools/Generate Elevator Level")]
 	public void GenerateElevatorLevel() {
@@ -14,6 +14,33 @@ public class GenerateLevel : MonoBehaviour {
 		// 3 must have 4 under it or no more bounds
 		// 5 should have 6 next to it and 7 / 8 under them 
 
+		int elevTileSqSize = 128;
+
+		int girdTileSqSize = 64;
+
+		// 1 to 2 rows of black tiles per every row of girders
+
+		bool [][] listOfTiles = new bool [10][];
+		for (int i = 0; i < 10; i++) {
+			listOfTiles [i] = new bool[10];
+			for (int j = 0; j < 10; j++) {
+				listOfTiles[i][j] = false;
+			}
+		}
+
+		// 1280x1280
+		for (int i = 0; i < 10; i++) {
+			int posX = (640 - 64) + (i * 128);
+			for (int j = 0; j < 10; j++) {
+				int posY = (640 - 64) + (j * 128);
+
+				Vector3 newPosition = new Vector3 (posX, posY, 0f);
+
+
+
+
+			}
+		}
 
 
 	}
