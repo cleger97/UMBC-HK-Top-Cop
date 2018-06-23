c10fe1006d0e4af8cbdc59f24e5343ddea47a301
cleger97 <chris.legerjr@gmail.com>
2018-06-23 16:31:29 -0400

    Fix Enemy/Dummy
    Added a target dummy (for testing Throwables) and fixed a severe error with TempEnemy.

---
+++ Assets/Scripts/TempEnemy/TargetDummy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetDummy : Enemy
+{
+    public override string returnName()
+    {
+        return "Target Dummy";
+    }
+
+    public override void takeDamage(int damage)
+    {
+        throw new System.NotImplementedException();
+    }
+
+}
